dae2435573e92e468dde0dc84aa575f9337c57a2
progress munoriarwa <40797405+progressmx@users.noreply.github.com>
2023-08-05 06:22:03 +0200

    Update vitalsSummary.tsx

--- app/componets/PatientComponents/vitalsSummary.tsx
+++ app/componets/PatientComponents/vitalsSummary.tsx
@@ -3,13 +3,13 @@ import { bundle } from '../types'
 
 export default function VitalsSummary(props: bundle) {
   return (
-    <div className='flex flex-row gap-4 w-full '>
+    <div className='flex flex-row gap-2 md:gap-4 w-full '>
         {
             props.input_bundle.map((vitals) => (
                 <div key={vitals.name} className={`flex flex-col justify-center place-content-center place-items-center
                  w-[33%] md:w-[20%] bg-white rounded-3xl p-6 ${props.className}`}>
                     <p className='mt-1 text-sm md:text-sm leading-5 text-gray-700'>{vitals.name}</p>
-                    <p className='text-lg md:text-xl font-semibold text-gray-900'>{vitals.label}</p>
+                    <p className='text-sm md:text-xl font-semibold text-gray-900'>{vitals.label}</p>
                 </div>
             ))
         }
