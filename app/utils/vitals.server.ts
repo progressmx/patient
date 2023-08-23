eb03a634f5a2fcf9bbc95e8a173497d894646e58
progress munoriarwa <40797405+progressmx@users.noreply.github.com>
2023-08-23 17:00:47 +0200

    Update vitals.server.ts

--- app/utils/vitals.server.ts
+++ app/utils/vitals.server.ts
@@ -41,6 +41,6 @@ export async function getVitals(ownerID: string,patientID: string)
       return patient
 
     } catch {
-      throw "no one found"
+       return {message: "no one found"}
     }
 }
